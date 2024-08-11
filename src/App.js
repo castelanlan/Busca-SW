24c4be66d3250976227335e794cfe127821a8cf3
Gabriel Castelan <gabrielmcastelan@gmail.com>
2024-08-11 16:43:21 -0300

    Commit inicial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+
+  function handleSearch(e) {
+    console.log(e.target.value)
+    setSearch(e.target.value)
+    fetch(`https://swapi.dev/api/people/?search=${e.target.value}`)
+      .then(data => data.json())
+      .then(data => {
+        console.log(data)
+        setData(data)
+        console.log("Filmes")
+      })
+  }
+
+  useEffect(() => {
+    fetch("https://swapi.dev/api/people/?search=")
+      .then(data => data.json())
+      .then(data => {
+        setData(data)
+      })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <input type='text' id='busca' value={search} onChange={handleSearch} autoFocus />
+
+      {Object.keys(data).length ? (
+        <div className='results-gallery'>
+          {data.results.map((personagem, index) => (
+            <div className={`results-${index}`}>
+              <p>{personagem.name}</p>
+              {/* <p>{personagem.height}</p> */}
+              {/* <p>{personagem.mass}</p> */}
+              {/* <p>{personagem.hair_color}</p> */}
+              {/* <p>{personagem.skin_color}</p> */}
+              <p>{personagem.birth_year}</p>
+              <p>{personagem.gender}</p>
+              <p>{personagem.eye_color}</p>
+              {/* <p>{personagem.homeworld}</p> */}
+              <p>{personagem.films}</p>
+              {/* <p>{personagem.species}</p> */}
+              {/* <p>{personagem.vehicles}</p> */}
+              {/* <p>{personagem.starships}</p> */}
+              {/* <p>{personagem.created}</p> */}
+              {/* <p>{personagem.edited}</p> */}
+              {/* <p>{personagem.url}</p> */}
+            </div>
+          ))
+          }
+        </div>
+      ) : (
+        <p>Busque pelo seu personagem</p>
+      )}
+    </div >
   );
 }
 
