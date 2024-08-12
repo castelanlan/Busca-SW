3812eeade7726612524cbe4f2c5f62d42eefd215
Gabriel Castelan <gabrielmcastelan@gmail.com>
2024-08-12 00:50:55 -0300

    Melhora na listagem

--- src/App.js
+++ src/App.js
@@ -2,53 +2,73 @@ import './App.css';
 import { useState, useEffect } from 'react';
 
 function App() {
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState([]);
   const [search, setSearch] = useState("");
+  const [people, setPeople] = useState([])
+  const [selec, setSelec] = useState([]);
 
-  function handleSearch(e) {
-    console.log(e.target.value)
-    setSearch(e.target.value)
-    fetch(`https://swapi.dev/api/people/?search=${e.target.value}`)
-      .then(data => data.json())
-      .then(data => {
-        console.log(data)
-        setData(data)
-        console.log("Filmes")
-      })
+  function handleSearch(event) {
+    const pesquisa = event.target.value.toLowerCase();
+    setSearch(pesquisa)
+
+    // console.log(people)
+    setSelec(
+      people.filter((person) => (
+        // console.log(person);
+        // console.log(pesquisa);
+        // console.log(person.toLowerCase().includes(pesquisa))
+        person.toLowerCase().includes(pesquisa)
+      ))
+    );
+
+    console.log(selec)
+
+
+    // people.forEach(p => p['id'] = p.url.split('/')[5]);
+
+    // console.log(people)
+    // console.log()
   }
 
   useEffect(() => {
-    fetch("https://swapi.dev/api/people/?search=")
-      .then(data => data.json())
-      .then(data => {
-        setData(data)
-      })
+    const personagens = [];
+
+    const fetchCharacters = async (url) => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`API request failed with status ${response.status}`);
+        }
+
+        const data = await response.json();
+        var personagem = (data.results) ? (data.results.map(person => person.name)) : (console.log(`DEU RUIM -> ${data}`))
+        personagens.push(...personagem);
+        console.log(personagens)
+
+        // Check for next page only if it exists
+        if (data.next) {
+          await fetchCharacters(data.next); // Recursive call to fetch next page
+        }
+        else {
+          setPeople(personagens)
+        }
+      } catch (error) {
+        console.error("Error fetching characters:", error);
+      }
+    };
+
+    fetchCharacters("https://swapi.py4e.com/api/people/"); // Call the function on component mount
   }, [])
 
   return (
     <div className="App">
       <input type='text' id='busca' value={search} onChange={handleSearch} placeholder='Pesquise aqui' autoFocus />
 
-      {Object.keys(data).length ? (
+      {selec.length ? (
         <div className='results-gallery'>
-          {data.results.map((personagem, index) => (
-            <div className={`results-${index}`}>
-              <p>{personagem.name}</p>
-              {/* <p>{personagem.height}</p> */}
-              {/* <p>{personagem.mass}</p> */}
-              {/* <p>{personagem.hair_color}</p> */}
-              {/* <p>{personagem.skin_color}</p> */}
-              {/* <p>{personagem.birth_year}</p> */}
-              {/* <p>{personagem.gender}</p> */}
-              {/* <p>{personagem.eye_color}</p> */}
-              {/* <p>{personagem.homeworld}</p> */}
-              {/* <p>{personagem.films}</p> */}
-              {/* <p>{personagem.species}</p> */}
-              {/* <p>{personagem.vehicles}</p> */}
-              {/* <p>{personagem.starships}</p> */}
-              {/* <p>{personagem.created}</p> */}
-              {/* <p>{personagem.edited}</p> */}
-              {/* <p>{personagem.url}</p> */}
+          {selec.map((personagem, index) => (
+            <div key={`results-${index}`}>
+              <p>{personagem}</p>
             </div>
           ))
           }
